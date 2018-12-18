600336f24a928c13507187468fb94b646f11dc36
shinymuuma <awittiwa@gmail.com>
2018-12-19 02:34:06 +0700

    Revert "Revert "Add Item""
    This reverts commit 711d3b1b5133381e3608ee445cf5de4322d61dcc.

--- src/aladdin/ui/addproduct/AddproductController.java
+++ src/aladdin/ui/addproduct/AddproductController.java
@@ -13,10 +13,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import org.apache.maven.Customer;
+import org.apache.persistence.HibernateUtil;
+import org.hibernate.Session;
+import org.apache.maven.Goods;
 
 /**
  * FXML Controller class
@@ -24,33 +32,83 @@ import javafx.stage.FileChooser.ExtensionFilter;
  * @author japan
  */
 public class AddproductController implements Initializable {
+
     @FXML
     private Button img;
     @FXML
     private ImageView imgv1;
+    @FXML
+    private TextField PdName;
+    @FXML
+    private TextField Price;
+    @FXML
+    private TextArea PdInfo;
+    @FXML
+    private Button Submit;
+    @FXML
+    private RadioButton sports;
+    @FXML
+    private ToggleGroup category;
+    @FXML
+    private RadioButton computer;
+    @FXML
+    private RadioButton Kids;
+    @FXML
+    private RadioButton Clothes;
+    @FXML
+    private RadioButton Tools;
+    @FXML
+    private RadioButton Other;
+    @FXML
+    private TextField amount;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
-    private void uploadimg(ActionEvent event) throws IOException{ 
+    private void uploadimg(ActionEvent event) throws IOException {
         FileChooser fc = new FileChooser();
         File selectedFile = fc.showOpenDialog(null);
         fc.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("Image Files",
                         "*.bmp", "*.png", "*.jpg", "*.gif"));
-        if(selectedFile != null){
+        if (selectedFile != null) {
             String imagepath = selectedFile.toURI().toURL().toString();
             Image image = new Image(imagepath);
             imgv1.setImage(image);
-        }
-        else{
+        } else {
             System.out.println("bad");
         }
     }
-    
+
+    @FXML
+    private void Submit(ActionEvent event) {
+        System.out.println("We're here");
+        
+        String info = PdInfo.getText();
+        String name = PdName.getText();
+        String price = Price.getText();
+        String quantiy = amount.getText();
+        System.out.println("Maven + Hibernate + MySQL");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        Goods goods = new Goods();
+
+        goods.setDetail(info);
+        goods.setName(name);
+        goods.setPrice(price);
+        goods.setQuantity(quantiy);
+        goods.setSeller("LungTu");
+
+        session.save(goods);
+        session.getTransaction().commit();
+        System.out.println("Added");
+    }
+
 }
