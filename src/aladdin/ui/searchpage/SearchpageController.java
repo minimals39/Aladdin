86f877ba18cb525dfe70cf52f1610b4b516c1d27
Jadsada <59010237@kmitl.ac.th>
2018-12-20 14:13:36 +0700

    searchfix

--- src/aladdin/ui/searchpage/SearchpageController.java
+++ src/aladdin/ui/searchpage/SearchpageController.java
@@ -7,9 +7,17 @@ package aladdin.ui.searchpage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.maven.Goods;
 
 /**
  * FXML Controller class
@@ -17,6 +25,20 @@ import javafx.fxml.Initializable;
  * @author japan
  */
 public class SearchpageController implements Initializable {
+    @FXML
+    private TextField keyW;
+    @FXML
+    private TableColumn<Goods, String> name;
+    @FXML
+    private TableColumn<Goods, String> price;
+    @FXML
+    private TableColumn<Goods, String> info;
+    @FXML
+    private Button search;
+    
+    private ObservableList<Goods> GoodsList = FXCollections.observableArrayList();
+    @FXML
+    private TableView<Goods> Table;
 
     /**
      * Initializes the controller class.
@@ -24,6 +46,11 @@ public class SearchpageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        info.setCellValueFactory(new PropertyValueFactory<>("detail"));
+        
+        Table.setItems(GoodsList);
     }    
 
     @FXML
