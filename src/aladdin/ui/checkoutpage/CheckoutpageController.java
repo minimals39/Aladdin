1ad4efd636a186b44a7d892acb6e7458e3b589f9
Jadsada <59010237@kmitl.ac.th>
2018-12-20 19:39:02 +0700

    receipt

--- src/aladdin/ui/checkoutpage/CheckoutpageController.java
+++ src/aladdin/ui/checkoutpage/CheckoutpageController.java
@@ -7,18 +7,22 @@ package aladdin.ui.checkoutpage;
 
 import aladdin.CustomerData;
 import aladdin.cart;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import org.apache.maven.Customer;
+import javafx.stage.Stage;
 import org.apache.maven.Goods;
 import org.apache.maven.Order;
 import org.apache.persistence.HibernateUtil;
@@ -113,9 +117,13 @@ public class CheckoutpageController implements Initializable {
         }
             
         ////
+        receipt();
         GoodsList.clear();
         incart.clear();
-       
+       closeStage();
+    }
+   private void closeStage() {
+        ((Stage) Table.getScene().getWindow()).close();
     }
 
     @FXML
@@ -124,5 +132,13 @@ public class CheckoutpageController implements Initializable {
         cart incart = cart.getInstance();
         incart.clear();
     }
-
+    private void receipt() throws IOException{
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/aladdin/ui/receipt/receipt.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        closeStage();
+    }
 }
---
+++ src/aladdin/ui/receipt/ReceiptController.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aladdin.ui.receipt;
+
+import aladdin.cart;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.maven.Goods;
+
+/**
+ * FXML Controller class
+ *
+ * @author japan
+ */
+public class ReceiptController implements Initializable {
+    @FXML
+    private TableView<Goods> Table;
+    @FXML
+    private TableColumn<Goods, String> iname;
+    @FXML
+    private TableColumn<Goods, String> price;
+    private ObservableList<Goods> GoodsList = FXCollections.observableArrayList();
+    @FXML
+    private Label total;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        // TODO
+
+        iname.setCellValueFactory(new PropertyValueFactory<>("name"));
+        price.setCellValueFactory(new PropertyValueFactory<>("price"));
+        cart incart = cart.getInstance();
+        if (!incart.getGoodsList().isEmpty()) {
+            GoodsList = incart.getGoodsList();
+            Table.setItems(GoodsList);
+        }
+        
+        int sum = 0;
+        
+        if(!incart.getGoodsList().isEmpty()){
+            int max = GoodsList.size(),i;
+            System.out.println(max);
+            for(i = 0; i<max ;i++){
+                System.out.println(GoodsList.get(i).getPrice());
+                sum += Integer.parseInt( GoodsList.get(i).getPrice());
+            }
+        
+        
+        }
+        
+        total.setText(""+sum);
+        System.out.println("sum = " +sum);
+        
+    }    
+    
+}
